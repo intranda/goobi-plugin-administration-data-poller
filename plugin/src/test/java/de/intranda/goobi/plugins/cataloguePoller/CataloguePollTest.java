b501543428ffa4a019ec4ae5458e35d12321fdb9
Michael Lippold <michael.lippold@intranda.com>
2022-09-22 17:20:01 +0200

    added new test class refs #23676

---
+++ plugin/src/test/java/de/intranda/goobi/plugins/cataloguePoller/CataloguePollTest.java
@@ -0,0 +1,10 @@
+package de.intranda.goobi.plugins.cataloguePoller;
+
+import org.junit.Test;
+
+public class CataloguePollTest {
+    @Test
+    public void updateMetsFileForProcessTest() {
+
+    }
+}
